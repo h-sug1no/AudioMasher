1e168585424d5eadbbcf0e01e1dcd103fa6defd3
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2021-06-25 12:42:23 +0900

    revert prev change.

--- static/masher.js
+++ static/masher.js
@@ -184,7 +184,7 @@ function upcateCandidateList(word='', cmWord) {
 		const vkeys = {};
 		editor.doc.eachLine((l) => {
 			l.text.split(/\s/).forEach((str) => {
-				if (str.match(/^_.+/) && word !== str) {
+				if (str.match(/^_.+/)) {
 					vkeys[str] = true;
 				}
 			});
